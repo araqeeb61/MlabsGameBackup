c53947c52ec97cffc09d65440c9e629ab44f54ea
Abdul Raqeeb <araqeeb618@gmail.com>
2024-08-04 00:53:43 +0500

    Project Updated

--- Assets/Scripts/DestroyOnCollison.cs
+++ Assets/Scripts/DestroyOnCollison.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 public class DestroyOnCollision : MonoBehaviour
 {
     [SerializeField ] GameObject Particles;
+    public float destryTime ;
     // This function is called when another collider enters the trigger collider attached to the object this script is attached to.
     void OnCollisionEnter(Collision collision)
     {
         // Check if the object colliding with this one is the player.
         if (collision.gameObject.tag == "Player")
         {
-            // Destroy this game object after 2 seconds.
-            Destroy(gameObject, 10.0f);
-             GameObject Exp=Instantiate(Particles,transform.position,transform.rotation);
-            Destroy(Exp,0.5f);
+            // Destroy this game object after certian time .
+            Destroy(gameObject, destryTime);
+             
             }        
     }
 }
